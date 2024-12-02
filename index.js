2fb6e3f68ffb548422522bc52a34803462dea640
Pradyumna <bhardwaj.pradyumn.pb@gmail.com>
2024-12-02 11:44:04 +0530

    index segregated

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
+//loading built-in packages
 const express = require('express');
 const path = require('path');
 const ejs = require('ejs');
+const cookieParser = require("cookie-parser");
+
+//loading local functions made
 const userRouter = require("./routes/user.js")
 const blogRouter = require("./routes/blog.js")
 const Blog = require("./models/blog.js")
 const mongoose = require("mongoose");
 const {returnDbUrl} = require("./pvt/dbUrl.js")
-const cookieParser = require("cookie-parser");
-const {
-    checkForAuthenticationCookie,
-  } = require("./middlewares/authentication");
+const {checkForAuthenticationCookie} = require("./middlewares/authentication");
 
 mongoose.connect(returnDbUrl()).then((e) => console.log("MongoDB Connected"));
 
@@ -22,9 +23,11 @@ app.use(cookieParser());
 app.use(checkForAuthenticationCookie("token"));
 app.use(express.static(path.resolve("./public")));
 
+//setting views
 app.set('view engine', 'ejs');
 app.set('views', path.resolve("./views"));
 
+//setting routes
 app.get("/", async (req, res) =>{
     const allBlogs = await Blog.find({});
     res.render("home", {
@@ -32,7 +35,6 @@ app.get("/", async (req, res) =>{
     blogs: allBlogs,
   });
 });
-
 app.use("/user", userRouter);
 app.use("/blog", blogRouter);
 
